511acb2b351d04cd9beebe1a428d839345958d33
red <gabrielfondato485@gmail.com>
2023-11-11 18:30:22 -0300

    it can change bytes now

--- main.cpp
+++ main.cpp
@@ -5,6 +5,28 @@ using namespace std;
 string option;
 string file;
 
+bool setByte(const string& filename, int addr, char val) {
+    // Open the file in binary mode
+    fstream target(filename, std::ios::binary | std::ios::in | std::ios::out);
+
+    if (!target.is_open()) {
+        cout << "Error opening file!" << endl;
+        return false;
+    }
+
+    // Move to the desired position in the file
+    target.seekp(addr);
+
+    // Set the value of the byte
+    target.write(&val, sizeof(char));
+
+    // Close the file
+    target.close();
+
+    cout << "Byte value set successfully!" << endl;
+    return true;
+}
+
 int main(){
     cout << "chose your option (read the README.md first)" << endl;
     cout << "1. Random corrupt" << endl;
@@ -12,13 +34,8 @@ int main(){
     cout << "3. Set a value for an adress" << endl;
     cin >> option;
 
-    cout << "what is the path of your file?" endl;
+    cout << "what is the path of your file?" << endl;
     cin >> file;
-    fstream target(file, std::ios::binary | std::ios::in | std::ios::out); //open file
-
-
+    setByte(file,10,12);
 }
 
-bool setByte(int addr, int val){
-
-}
